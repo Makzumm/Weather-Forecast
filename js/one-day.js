c990f77bf1b6817c1fd4f9391a9991c913d99f16
Maksym <106184045+Makzumm@users.noreply.github.com>
2023-05-04 20:13:42 +0200

    Fix

--- js/one-day.js
+++ js/one-day.js
@@ -6,6 +6,7 @@ let feelsLike = document.querySelector('.feels-like');
 let windSpeed = document.querySelector('.wind-speed');
 let description = document.querySelector('.description');
 let weatherWrapper = document.querySelector('.weather-block');
+let weatherWrapperEls = document.querySelectorAll('.child-block')
 let errorField = document.querySelector('.error-field');
 let errorInputField = document.querySelector('.error-input-field');
 
@@ -15,23 +16,51 @@ let townValue = undefined;
 
 buttonEl.addEventListener('click', fetchTown);
 
-document.getElementById('search-input').addEventListener('keyup', function (e) {
-    townValue = inputEl.value
+inputEl.addEventListener('keyup', function (e) {
 
-    if (e.code === 'Enter') {
+    if (inputEl.value === '') {
+        return console.log(false)
+    } else {
+        townValue = inputEl.value
 
-        buttonEl.click();
+        if (e.code === 'Enter') {
 
-        setTimeout(() => {
+            buttonEl.click();
 
-            if (cityName.value !== '') {
-                inputEl.value = '';
-            }
-        }, 200)
-        errorField.innerHTML = '';
-        errorInputField.innerHTML = '';
+            setTimeout(() => {
+
+                if (cityName.value !== '') {
+                    inputEl.value = '';
+                }
+            }, 200)
+
+            errorField.innerHTML = '';
+            errorInputField.innerHTML = '';
+        }
     }
 })
+
+function errorFilter(error) {
+    let errorEl = error.name
+
+    if (errorEl) {
+        for (const el of weatherWrapperEls) {
+            el.innerHTML = '';
+        }
+    }
+
+    errorField.innerHTML = 'Something went wrong, try again';
+    errorInputField.innerHTML = '';
+}
+
+function townMarkUp(data) {
+    cityName.innerHTML = `<p class="weather-block__txt">City : <span class="weather-block__txt--marked">${townValue.charAt(0).toUpperCase() + townValue.slice(1).toLowerCase()}</span></p>`;
+    temp.innerHTML = `<p class="weather-block__txt">Temperature : <span class="weather-block__txt--marked">${Math.round(data['main']['temp'])}&#176;C</span></p>`;
+    feelsLike.innerHTML = `<p class="weather-block__txt">Feels like : <span class="weather-block__txt--marked">${Math.round(data['main']['feels_like'])}&#176;C</span></p>`;
+    windSpeed.innerHTML = `<p class="weather-block__txt">Wind speed : <span class="weather-block__txt--marked">${data['wind']['speed']}km/h</span></p>`;
+    description.innerHTML = `<p class="weather-block__txt">Weather : <span class="weather-block__txt--marked">${data['weather'][0]['description'].charAt(0).toUpperCase() + data['weather'][0]['description'].slice(1)}</span></p><img src='http://openweathermap.org/img/w/${data["weather"][0]["icon"]}.png' width='65' height='65'>`;
+    console.log(data)
+}
 
 function fetchTown() {
 
@@ -44,16 +73,10 @@ function fetchTown() {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${townValue}&appid=5161531edb6939420a9faddefc0dd57d&units=metric`)
             .then(response => response.json())
             .then(data => {
-                cityName.innerHTML = `<p class="weather-block__txt">City : <span class="weather-block__txt--marked">${townValue.charAt(0).toUpperCase() + townValue.slice(1).toLowerCase()}</span></p>`;
-                temp.innerHTML = `<p class="weather-block__txt">Temperature : <span class="weather-block__txt--marked">${Math.round(data['main']['temp'])}&#176;C</span></p>`;
-                feelsLike.innerHTML = `<p class="weather-block__txt">Feels like : <span class="weather-block__txt--marked">${Math.round(data['main']['feels_like'])}&#176;C</span></p>`;
-                windSpeed.innerHTML = `<p class="weather-block__txt">Wind speed : <span class="weather-block__txt--marked">${data['wind']['speed']}km/h</span></p>`;
-                description.innerHTML = `<p class="weather-block__txt">Weather : <span class="weather-block__txt--marked">${data['weather'][0]['description'].charAt(0).toUpperCase() + data['weather'][0]['description'].slice(1)}</span></p><img src='http://openweathermap.org/img/w/${data["weather"][0]["icon"]}.png' width='65' height='65'>`;
-                console.log(data)
+                townMarkUp(data);
             })
             .catch(error => {
-                errorField.innerHTML = error;
-                errorInputField.innerHTML = '';
+                errorFilter(error)
             });
 
     }
