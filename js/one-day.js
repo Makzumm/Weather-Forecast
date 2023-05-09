070865a2b0372fc2499a56453f110a6079dde28e
Maksym <106184045+Makzumm@users.noreply.github.com>
2023-05-09 22:03:07 +0200

    Fixed some bugs

--- js/one-day.js
+++ js/one-day.js
@@ -6,10 +6,17 @@ let feelsLike = document.querySelector('.feels-like');
 let windSpeed = document.querySelector('.wind-speed');
 let description = document.querySelector('.description');
 let weatherWrapper = document.querySelector('.weather-block');
-let weatherWrapperEls = document.querySelectorAll('.child-block')
+let weatherWrapperEls = document.querySelectorAll('.child-block');
 let errorField = document.querySelector('.error-field');
 let errorInputField = document.querySelector('.error-input-field');
-let formSubm = document.querySelector('.form-subm')
+let locationBtn = document.querySelector('.location-button');
+let loadingGif = document.querySelector('.loading_gif');
+
+let townValue = undefined;
+
+let reverseGeocoder = new BDCReverseGeocode();
+let geoLocatCityName = undefined;
+
 
 const API_KEY = "5161531edb6939420a9faddefc0dd57d";
 const FETCH_LINK = "https://api.openweathermap.org/data/2.5/weather?";
@@ -18,32 +25,49 @@ const FETCH_LINK = "https://api.openweathermap.org/data/2.5/weather?";
 
 /////////////////////////////////////////////////////////////////
 
-const locationBtn = document.querySelector('.location-button');
+function onSuccess() {
+    console.log(true);
+}
 
-let reverseGeocoder = new BDCReverseGeocode();
-
-let geoLocatCityName = undefined
-
-reverseGeocoder.localityLanguage = 'es';
+function onError() {
+    console.log(false);
+}
 
 function getLocation(result) {
 
     geoLocatCityName = result.city;
 
-    inputEl.value = geoLocatCityName;
+    townValue = geoLocatCityName;
+
+    inputEl.value = townValue;
+}
+
+function navigatorPermissionCheck() {
+
+    if (navigator.permissions) {
+
+        navigator.permissions.query({ name: 'geolocation' }).then(function (permissionStatus) {
+            if (permissionStatus.state === "denied") {
+
+                console.log('denied');
+
+            } else {
+                inputEl.focus();
+                reverseGeocoder.getClientLocation(getLocation);
+            }
+        });
+    }
+
 }
 
 locationBtn.addEventListener('click', () => {
+    if (!navigator.geolocation) {
 
-    if (navigator.permissions) {
-        navigator.permissions.query({ name: 'geolocation' }).then(function (permissionStatus) {
-            if (permissionStatus.state === 'denied') {
-                reverseGeocoder.getClientCoordinates = false
-                console.log('denied')
-            } else {
-                reverseGeocoder.getClientLocation(getLocation)
-            }
-        });
+        console.log("navigator is not supported");
+
+    } else {
+        // navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        navigatorPermissionCheck();
     }
 
 })
@@ -52,15 +76,53 @@ locationBtn.addEventListener('click', () => {
 
 ////////////////////////////////////////////////////////////////
 
-let townValue = undefined;
+function errorFieldsCleaner() {
+    errorField.innerHTML = '';
+    errorInputField.innerHTML = '';
+}
+
+function errorFilter(error) {
+    let errorEl = error.name;
+
+    if (errorEl) {
+        for (const el of weatherWrapperEls) {
+            el.innerHTML = '';
+        }
+    }
+
+    errorField.innerHTML = 'Something went wrong, please, try again.';
+    errorInputField.innerHTML = '';
+}
+
+function townMarkUp(data) {
+    cityName.innerHTML = `<p class="weather-block__txt">City : <span class="weather-block__txt--marked">${townValue.charAt(0).toUpperCase() + townValue.slice(1).toLowerCase()}</span></p>`;
+    temp.innerHTML = `<p class="weather-block__txt">Temperature : <span class="weather-block__txt--marked">${Math.round(data['main']['temp'])}&#176;C</span></p>`;
+    feelsLike.innerHTML = `<p class="weather-block__txt">Feels like : <span class="weather-block__txt--marked">${Math.round(data['main']['feels_like'])}&#176;C</span></p>`;
+    windSpeed.innerHTML = `<p class="weather-block__txt">Wind speed : <span class="weather-block__txt--marked">${data['wind']['speed']}km/h</span></p>`;
+    description.innerHTML = `<p class="weather-block__txt">Weather : <span class="weather-block__txt--marked">${data['weather'][0]['description'].charAt(0).toUpperCase() + data['weather'][0]['description'].slice(1)}</span></p><img class='icon-weather' src='http://openweathermap.org/img/w/${data["weather"][0]["icon"]}.png' width='65' height='65'>`;
+}
+
+function inputCleaner() {
+
+    setTimeout(() => {
+
+        if (cityName.value !== '') {
+            inputEl.value = '';
+        }
+    }, 200)
+
+}
 
 buttonEl.addEventListener('click', fetchTown);
 
 inputEl.addEventListener('keyup', function (e) {
 
     if (inputEl.value === '') {
-        return null
+
+        return null;
+
     } else {
+
         townValue = inputEl.value
 
         if (e.code === 'Enter') {
@@ -74,66 +136,37 @@ inputEl.addEventListener('keyup', function (e) {
                 }
             }, 200)
 
-            errorField.innerHTML = '';
-            errorInputField.innerHTML = '';
+            inputEl.blur()
+
+            errorFieldsCleaner();
         }
     }
 })
 
-function errorFilter(error) {
-    let errorEl = error.name
-
-    if (errorEl) {
-        for (const el of weatherWrapperEls) {
-            el.innerHTML = '';
-        }
-    }
-
-    errorField.innerHTML = 'Something went wrong, please, try again.';
-    errorInputField.innerHTML = '';
-}
-
-function townMarkUp(data) {
-
-    cityName.innerHTML = `<p class="weather-block__txt">City : <span class="weather-block__txt--marked">${townValue.charAt(0).toUpperCase() + townValue.slice(1).toLowerCase()}</span></p>`;
-    temp.innerHTML = `<p class="weather-block__txt">Temperature : <span class="weather-block__txt--marked">${Math.round(data['main']['temp'])}&#176;C</span></p>`;
-    feelsLike.innerHTML = `<p class="weather-block__txt">Feels like : <span class="weather-block__txt--marked">${Math.round(data['main']['feels_like'])}&#176;C</span></p>`;
-    windSpeed.innerHTML = `<p class="weather-block__txt">Wind speed : <span class="weather-block__txt--marked">${data['wind']['speed']}km/h</span></p>`;
-    description.innerHTML = `<p class="weather-block__txt">Weather : <span class="weather-block__txt--marked">${data['weather'][0]['description'].charAt(0).toUpperCase() + data['weather'][0]['description'].slice(1)}</span></p><img class='icon-weather' src='http://openweathermap.org/img/w/${data["weather"][0]["icon"]}.png' width='65' height='65'>`;
-    console.log(data)
-}
-
 function fetchTown() {
 
-    errorField.innerHTML = ""
-
-    errorInputField.innerHTML = ""
-
-    setTimeout(() => {
-
-        if (cityName.value !== '') {
-            inputEl.value = '';
-        }
-    }, 200)
+    errorFieldsCleaner();
+    inputCleaner();
 
     if (inputEl.value === '') {
-
         errorInputField.innerHTML = 'Please, type the city name!';
 
     } else {
+
+        loadingGif.classList.remove('loading_gif--hidden');
 
         fetch(`${FETCH_LINK}q=${townValue}&appid=${API_KEY}&units=metric`)
             .then(response => response.json())
             .then(data => {
                 townMarkUp(data);
+
+                loadingGif.classList.add('loading_gif--hidden');
             })
             .catch(error => {
-                errorFilter(error)
+                errorFilter(error);
             });
 
     }
 }
 
 /////////////////////////////////////////////////////////////////
-
-
