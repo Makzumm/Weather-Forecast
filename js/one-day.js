8737a18359347cc06e1da8e59a3d6186c985c16e
Maksym <106184045+Makzumm@users.noreply.github.com>
2024-01-08 22:56:18 +0100

    Something 4.0

--- js/one-day.js
+++ js/one-day.js
@@ -79,7 +79,11 @@ function errorFilter(error) {
 }
 
 function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    const words = str.split(' ');
+    const capitalizedWords = words.map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+    return capitalizedWords.join(' ');
 }
 
 function buildWeatherInfo(title, value) {
